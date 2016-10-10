db302c5c5242448f28dc1b95517e79b9849c27c7
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-10-10 15:08:19 -0700

    Added smooth scroll to the build

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/ngSmoothScroll/lib/angular-smooth-scroll.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
