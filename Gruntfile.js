c7afdd6ce7f2fcf824e4134138350d8d0f5bfd42
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-10-10 15:14:47 -0700

    testing a deletion

--- Gruntfile.js
+++ Gruntfile.js
@@ -385,7 +385,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
